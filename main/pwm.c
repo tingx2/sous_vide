0e4991adcf15aeae1ac8e71dc05f98844330259c
daidavid <pi@raspberrypi.(none)>
2014-06-09 01:35:14 +0000

    modify makefile to include pwm and temp

--- main/pwm.c
+++ main/pwm.c
@@ -9,32 +9,40 @@ static uint32 freq_hz; //Frequency for PWM
 static uint32 time_high_thres;
 static uint32 time_low_thres;
 
+static uint32 time_high_thres_incre;
+static uint32 time_low_thres_incre;
 
-void pwm_init()
+void pwm_init(uint32 freq)
 {
-  freq_hz=100;
+  freq_hz=freq;
 	int ret_val= bcm2835_init();
   // add error handling
 
 	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
 }
 
-void pwm_run(uint32 duty_cycle)
+void pwm_run()
 {
-	int cur_time = read_timer32();
-	if(cur_time>time_high_thres && cur_time-time_high_thres < 0xFFFFFFFF/2)
+	uint32 cur_time = read_timer32();
+	if(cur_time - time_high_thres < 0xFFFFFFFF/2)
 	{
-		pwm_update_duty(duty_cycle);
+		pwm_timer_update();
 		bcm2835_gpio_write(PIN, HIGH);
 	}
-	else if (cur_time>time_low_thres && cur_time - time_low_thres < 0xFFFFFFFF/2)
+	else if (cur_time - time_low_thres < 0xFFFFFFFF/2)
 		bcm2835_gpio_write(PIN, LOW);
 
 }
 
+void pwm_timer_update()
+{	
+	uint32 cur_time = read_timer32(); 
+	time_high_thres= cur_time + time_high_thres_incre; //Toggle LOW to HIGH end of period.
+	time_low_thres= cur_time + time_low_thres_incre; //Toggle HIGH to LOW end of period.
+}
+
 void pwm_update_duty(uint32 duty_cycle)
 {
-	int cur_time = read_timer32(); 
-	time_high_thres= cur_time+(100000/freq_hz); //Toggle LOW to HIGH end of period.
-	time_low_thres= cur_time+(1000/freq_hz*duty_cycle); //Toggle LOW to HIGH end of period.
+	time_high_thres_incre= (100000/freq_hz); 
+	time_low_thres_incre= (1000/freq_hz*duty_cycle); 
 }
